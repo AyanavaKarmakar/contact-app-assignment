2120234b05f6e02af4511d511ce1e539f791e2bc
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-06-14 19:13:01 +0530

    feat: add base searchbar

--- App.tsx
+++ App.tsx
@@ -1,18 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, View } from 'react-native'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { SearchBar } from './components'
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient()
 
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <View style={styles.container}>
-        <Text>Contact App</Text>
+        <View style={styles.searchContainer}>
+          <SearchBar />
+        </View>
+
         <StatusBar style="auto" />
       </View>
     </QueryClientProvider>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -20,6 +24,10 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
-});
+
+  searchContainer: {
+    width: '100%',
+  },
+})
---
+++ components/searchBar.tsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import { View, TextInput, StyleSheet } from 'react-native'
+import { AntDesign, FontAwesome } from '@expo/vector-icons'
+
+export function SearchBar() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.searchSection}>
+        <AntDesign
+          name="search1"
+          size={22}
+          color="#999"
+          style={styles.searchIcon}
+        />
+
+        <TextInput
+          style={styles.input}
+          placeholder="Search"
+          placeholderTextColor="#000000"
+        />
+
+        {/* use 'sort-amount-asc' when sort option is flipped */}
+        <FontAwesome
+          name="sort-amount-desc"
+          size={20}
+          color="#000"
+          style={styles.sortIcon}
+        />
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    padding: 10,
+  },
+
+  searchSection: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#f0f0f0',
+    borderRadius: 1,
+  },
+
+  searchIcon: {
+    paddingHorizontal: 15,
+    paddingVertical: 5,
+  },
+
+  input: {
+    flex: 1,
+    paddingTop: 10,
+    paddingRight: 10,
+    paddingBottom: 10,
+    paddingLeft: 0,
+    backgroundColor: '#f0f0f0',
+    color: '#424242',
+    fontSize: 18,
+  },
+
+  sortIcon: {
+    paddingHorizontal: 15,
+    paddingVertical: 5,
+  },
+})
